0da891d4f41e5fb527fc7d8a01ff8fb950ab1310
KonradOrzelowski <109598256+KonradOrzelowski@users.noreply.github.com>
2023-03-12 16:54:06 +0100

    Add ngfor

--- frontend/src/app/page-footballer-details/page-footballer-details.component.ts
+++ frontend/src/app/page-footballer-details/page-footballer-details.component.ts
@@ -15,39 +15,41 @@ export class PageFootballerDetailsComponent implements OnInit {
   similar_footballers_insta : any;
   similar_footballers_sofifa: any;
 
-  constructor(private route: ActivatedRoute, private http: HttpClient, private dataService: DataService) { }
+  constructor(private route: ActivatedRoute, private http: HttpClient, private dataService: DataService) {
+    
+  }
 
   async ngOnInit() {
     console.log('++++++++++++++++++');
-    this.data = this.dataService.getData(); 
-    
-    this.similar_footballers_insta  = await this.fetch_http_endpoint(`http://127.0.0.1:5000/similarity/insta/${this.data}`);
-    this.similar_footballers_sofifa = await this.fetch_http_endpoint(`http://127.0.0.1:5000/similarity/sofifa/${this.data}`);
-
-    this.similar_footballers_insta  = this.string2array(this.similar_footballers_insta);
-    this.similar_footballers_sofifa = this.string2array(this.similar_footballers_sofifa);
-
-    console.log(this.data);
-    console.log(this.similar_footballers_insta);
-    console.log(this.similar_footballers_sofifa);
-    console.log('++++++++++++++++++');
-    
-
-
+    this.data = this.dataService.getData();
+  
+    const similar_footballers_insta_str:any = await this.fetch_http_endpoint(`http://127.0.0.1:5000/similarity/insta/${this.data}`);
+    this.similar_footballers_insta = this.string2array(similar_footballers_insta_str);
+  
+    const similar_footballers_sofifa_str:any = await this.fetch_http_endpoint(`http://127.0.0.1:5000/similarity/sofifa/${this.data}`);
+    this.similar_footballers_sofifa = this.string2array(similar_footballers_sofifa_str);
   }
-  string2array(inputString: string) {
-    try {
-      inputString = inputString.replace(/"/g, '');
-      const regex = /["{](.*?)[:](.*?)[,}]/g;
-      const matches = inputString.matchAll(regex);
-
-      const dataDictArray = Array.from(matches, match => ({ 'Name': match[1], 'Score': parseFloat(match[2]) }));
-      return dataDictArray;
-    } catch (error) {
-      console.error(error);
-      return [];
+  
+  string2array(str2transform: string) {
+    str2transform = str2transform.replace(/"/g, '');
+    str2transform = str2transform.replace(/{/g, '');
+    str2transform = str2transform.replace(/}/g, '');
+  
+    const temp_array = str2transform.split(",");
+  
+    const output = [];
+    for (let i = 0; i < temp_array.length; i++){
+        const [name, dist] =  temp_array[i].split(":");
+        output.push([name, parseFloat(dist)]);
     }
+  
+    return output;
   }
+  
+  
+
+
+
   
   async fetch_http_endpoint(url: string) {
     const response = await this.http.get(url).toPromise();
