6fb25fe9815b3e0bdb578318da2caa2537a4b3ee
KonradOrzelowski <109598256+KonradOrzelowski@users.noreply.github.com>
2023-09-23 15:05:06 +0200

    Test

--- backend/test_app.py
+++ backend/test_app.py
@@ -7,7 +7,6 @@ from connect_mysql import MySQLConnection
 from app_flask import app
 
 class TestFlaskApp(unittest.TestCase):
-
     def setUp(self):
         app.testing = True
         self.app = app.test_client()
@@ -17,4 +16,15 @@ class TestFlaskApp(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
 
 if __name__ == '__main__':
-    unittest.main()
+    # Create a TestResult object to store the test results
+    result = unittest.TestResult()
+
+    # Create a TestSuite object containing the TestFlaskApp class
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestFlaskApp)
+
+    # Run the tests and store the result in the status variable
+    status = suite.run(result)
+
+    # Print the status of the tests
+    print("Test Status: ", status.wasSuccessful())
+
